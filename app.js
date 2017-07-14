9761c8d4fb56dee51cc3863b99a8007d51f9b091
parkjunyoung <pjoon86@naver.com>
2017-07-14 23:26:44 +0900

    라우팅 추가

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 
+var admin = require('./routes/admin');
+
 var app = express();
 var port = 3000;
 
@@ -7,6 +9,8 @@ var port = 3000;
 app.get('/', function(req,res){
     res.send('first app');
 });
+// Routing
+app.use('/admin', admin);
 
 app.listen( port, function(){
     console.log('Express listening on port', port);
