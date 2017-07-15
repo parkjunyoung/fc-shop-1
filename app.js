a0a10ec2d53594e62ba8a518f4bec845d9bdd70e
parkjunyoung <pjoon86@naver.com>
2017-07-15 11:37:48 +0900

    제품등록 작성

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 //MongoDB 접속
 var mongoose = require('mongoose');
@@ -23,6 +25,10 @@ var port = 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// 미들웨어 셋팅
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req,res){
     res.send('first app');
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var ProductsModel = require('../models/ProductsModel');
 
 router.get('/', function(req,res){
     res.send('admin app');
@@ -9,6 +10,21 @@ router.get('/products', function(req,res){
     res.render( 'admin/products' , 
         { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
     );
+});
+
+router.get('/products/write', function(req,res){
+    res.render( 'admin/form');
+});
+
+router.post('/products/write', function(req,res){
+    var product = new ProductsModel({
+        name : req.body.name,
+        price : req.body.price,
+        description : req.body.description,
+    });
+    product.save(function(err){
+        res.redirect('/admin');
+    });
 });
 
 module.exports = router;
