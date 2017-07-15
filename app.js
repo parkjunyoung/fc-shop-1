30a844262c78305e6e83cdf84db62e2269adc75d
parkjunyoung <pjoon86@naver.com>
2017-07-15 10:11:33 +0900

    모델 생성

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 var express = require('express');
 
+//MongoDB 접속
+var mongoose = require('mongoose');
+var autoIncrement = require('mongoose-auto-increment');
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+    console.log('mongodb connect');
+});
+
+var connect = mongoose.connect('mongodb://127.0.0.1:27017/fastcampus', { useMongoClient: true });
+autoIncrement.initialize(connect);
+
 var admin = require('./routes/admin');
 
 var app = express();
