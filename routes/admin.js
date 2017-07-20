68298bcf764a01ebc586b37f0b727aa4269d2394
parkjunyoung <pjoon86@naver.com>
2017-07-20 15:45:49 +0900

    제품수정

--- routes/admin.js
+++ routes/admin.js
@@ -36,4 +36,25 @@ router.get('/products/detail/:id' , function(req, res){
     });
 });
 
+router.get('/products/edit/:id' ,function(req, res){
+    //기존에 폼에 value안에 값을 셋팅하기 위해 만든다.
+    ProductsModel.findOne({ id : req.params.id } , function(err, product){
+        res.render('admin/form', { product : product });
+    });
+});
+
+router.post('/products/edit/:id', function(req, res){
+    //넣을 변수 값을 셋팅한다
+    var query = {
+        name : req.body.name,
+        price : req.body.price,
+        description : req.body.description,
+    };
+
+    //update의 첫번째 인자는 조건, 두번째 인자는 바뀔 값들
+    ProductsModel.update({ id : req.params.id }, { $set : query }, function(err){
+        res.redirect('/admin/products/detail/' + req.params.id ); //수정후 본래보던 상세페이지로 이동
+    });
+});
+
 module.exports = router;
