f4b979ca97279a7238a90bf6129f858280726054
parkjunyoung <pjoon86@naver.com>
2017-07-20 15:17:44 +0900

    상세페이지 작성

--- routes/admin.js
+++ routes/admin.js
@@ -29,4 +29,11 @@ router.post('/products/write', function(req,res){
     });
 });
 
+router.get('/products/detail/:id' , function(req, res){
+    //url 에서 변수 값을 받아올떈 req.params.id 로 받아온다
+    ProductsModel.findOne( { 'id' :  req.params.id } , function(err ,product){
+        res.render('admin/productsDetail', { product: product });  
+    });
+});
+
 module.exports = router;
