a736e0bc2fc6e1188e00254916a22aaa7ad7db2a
parkjunyoung <pjoon86@naver.com>
2017-07-22 00:34:47 +0900

    csurf 적용

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 //MongoDB 접속
 var mongoose = require('mongoose');
@@ -30,6 +31,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get('/', function(req,res){
     res.send('first app');
--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var ProductsModel = require('../models/ProductsModel');
 var CommentsModel = require('../models/CommentsModel');
 
+// csrf 셋팅
+var csrf = require('csurf');
+var csrfProtection = csrf({ cookie: true });
+
 router.get('/', function(req,res){
     res.send('admin app');
 });
@@ -15,12 +19,12 @@ router.get('/products', function(req,res){
     });
 });
 
-router.get('/products/write', function(req,res){
+router.get('/products/write', csrfProtection , function(req,res){
     //edit에서도 같은 form을 사용하므로 빈 변수( product )를 넣어서 에러를 피해준다
-    res.render( 'admin/form' , { product : "" }); 
+    res.render( 'admin/form' , { product : "", csrfToken : req.csrfToken() }); 
 });
 
-router.post('/products/write', function(req,res){
+router.post('/products/write', csrfProtection, function(req,res){
     var product = new ProductsModel({
         name : req.body.name,
         price : req.body.price,
@@ -46,14 +50,14 @@ router.get('/products/detail/:id' , function(req, res){
     });
 });
 
-router.get('/products/edit/:id' ,function(req, res){
+router.get('/products/edit/:id' , csrfProtection ,function(req, res){
     //기존에 폼에 value안에 값을 셋팅하기 위해 만든다.
     ProductsModel.findOne({ id : req.params.id } , function(err, product){
-        res.render('admin/form', { product : product });
+        res.render('admin/form', { product : product, csrfToken : req.csrfToken() });
     });
 });
 
-router.post('/products/edit/:id', function(req, res){
+router.post('/products/edit/:id', csrfProtection, function(req, res){
     //넣을 변수 값을 셋팅한다
     var query = {
         name : req.body.name,
